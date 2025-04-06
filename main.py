0b819a7cdea39decd1125d16b32018b1cb5e1f84
Sean Ruth <Seanruth27@gmail.com>
2025-04-05 21:39:26 -0700

    player added with draw

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import pygame
 from constants import *
+from player import *
 
 def main(): 
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x, y, PLAYER_RADIUS)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, 000000)
+        
+        #fill screen with color black
+        screen.fill((0, 0, 0))
+        #draw player 
+        player.draw(screen)
+        #flip display
         pygame.display.flip()
-        clock.tick(60)
-        dt = clock.tick() / 1000
+        #maintain 60fps
+        dt = clock.tick(60) / 1000
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
